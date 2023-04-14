7179c78c8cfb2007c82ab4696f29c2949b45b5ca
markocur <mariusz.m.koch@gmail.com>
2023-04-14 13:15:38 +0000

    feat: display user info before game

--- number_guess.sh
+++ number_guess.sh
@@ -2,7 +2,7 @@
 
 PSQL="psql -X --username=freecodecamp --dbname=number_guess --tuples-only -c"
 
-echo -e "Enter your username"
+echo -e "Enter your username:"
 read USERNAME
 CHECK_USER=$($PSQL "select username from users where username='$USERNAME'")
 
@@ -10,7 +10,12 @@ if [[ -z $CHECK_USER ]]
 then
   #add user to database
   ADD_USER=$($PSQL "insert into users values('$USERNAME',0,0)")
+  echo "Welcome, $USERNAME! It looks like this is your first time here."
 else
   # greet user and give info about his games
-  echo Hello, $USERNAME!
+  USER_DATA=$($PSQL "select * from users where username='$USERNAME'")
+  echo "$USER_DATA" | while read USERNAME BAR GAMES BAR BEST 
+  do
+    echo "Welcome back, $USERNAME! You have played $GAMES games, and your best game took $BEST guesses."
+  done
 fi
