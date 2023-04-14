42daed2e80e8790f5a15474d35e7309f3030b9e4
markocur <mariusz.m.koch@gmail.com>
2023-04-14 12:57:52 +0000

    feat: check if user exists, if not add them to DB

--- number_guess.sh
+++ number_guess.sh
@@ -1,1 +1,16 @@
 #!/bin/bash
+
+PSQL="psql -X --username=freecodecamp --dbname=number_guess --tuples-only -c"
+
+echo -e "Enter your username"
+read USERNAME
+CHECK_USER=$($PSQL "select username from users where username='$USERNAME'")
+
+if [[ -z $CHECK_USER ]]
+then
+  #add user to database
+  ADD_USER=$($PSQL "insert into users values('$USERNAME',0,0)")
+else
+  # greet user and give info about his games
+  echo Hello, $USERNAME!
+fi
